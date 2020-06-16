187ea34cd0dbe24b17b92169a5a7dece24234f29
Gabriel ufot <gabrielufot23@gmail.com>
2020-06-16 07:32:20 +0100

    postquestion model

--- page/models.py
+++ page/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils.text import slugify
+from django.conf import settings
 
-# Create your models here.
+
+User = settings.AUTH_USER_MODEL
+
+
+class PostQuestion(models.Model):
+    """ Model for posting questions """
+    title = models.CharField(max_length=100, unique=True)
+    user = models.ForeignKey(User, null=False, blank=False, on_delete=models.CASCADE)
+    slug = models.SlugField(unique=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    text_content = models.TextField()
+
+    def __str__(self):
+        return self.title
--- page/views.py
+++ page/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+# from django.conf import settings
+
+
+# User = settings.AUTH_USER_MODEL
 
 
 def index(request):
