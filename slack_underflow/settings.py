a499382a22ab152f20e83c9128004c10bae22105
Gabriel ufot <gabrielufot23@gmail.com>
2020-07-12 16:08:21 +0100

    integrating postgreSQL

--- slack_underflow/settings.py
+++ slack_underflow/settings.py
@@ -108,8 +108,13 @@ WSGI_APPLICATION = 'slack_underflow.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'djangodb',
+        'USER': 'stackoverflow',
+        'PASSWORD': 'gaboski23',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
