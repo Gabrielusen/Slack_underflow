3cc11fdb3c9a97790cc9c3d0e1a8def07873bc91
Gabriel ufot <gabrielufot23@gmail.com>
2020-07-24 14:25:24 +0100

    make changes

--- slack_underflow/settings.py
+++ slack_underflow/settings.py
@@ -12,6 +12,13 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+
+# Configure Django App for Heroku.
+import django_heroku
+
+
+django_heroku.settings(locals())
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -27,9 +34,9 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['slackunderflow.herokuapp.com']
 
-import django_heroku
+# import django_heroku
 
-django_heroku.core.settings(locals())
+# django_heroku.core.settings(locals())
 
 
 TINYMCE_DEFAULT_CONFIG = {
